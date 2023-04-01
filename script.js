769f835c641251daa0bc3d0f740e7625e469305c
Lai03 <123115328+Lai03@users.noreply.github.com>
2023-04-01 16:17:43 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -10,5 +10,27 @@ function replaceImages() {
   }
 }
 
-// Run the replaceImages function when the page loads
-window.onload = replaceImages;
+// This function listens for changes to the page's DOM and calls replaceImages
+// whenever new nodes are added to the page
+function observePage() {
+  // Create a new MutationObserver to listen for changes to the page's DOM
+  let observer = new MutationObserver(function(mutations) {
+    // Loop through all mutations
+    for (let i = 0; i < mutations.length; i++) {
+      // Check if new nodes were added
+      if (mutations[i].addedNodes.length > 0) {
+        // Call replaceImages to replace any new images on the page
+        replaceImages();
+      }
+    }
+  });
+
+  // Start observing the page's DOM for changes
+  observer.observe(document.body, { childList: true, subtree: true });
+}
+
+// Run the replaceImages and observePage functions when the page loads
+window.onload = function() {
+  replaceImages();
+  observePage();
+};
