ea9526642ac4768ed6628022c17c5ef56b55d007
nNoah187 <113475241+nNoah187@users.noreply.github.com>
2023-04-01 16:20:50 -0500

    Yobama loads on new images updated in the page

--- script.js
+++ script.js
@@ -10,27 +10,18 @@ function replaceImages() {
   }
 }
 
-// This function listens for changes to the page's DOM and calls replaceImages
-// whenever new nodes are added to the page
-function observePage() {
-  // Create a new MutationObserver to listen for changes to the page's DOM
-  let observer = new MutationObserver(function(mutations) {
-    // Loop through all mutations
-    for (let i = 0; i < mutations.length; i++) {
-      // Check if new nodes were added
-      if (mutations[i].addedNodes.length > 0) {
-        // Call replaceImages to replace any new images on the page
-        replaceImages();
-      }
-    }
-  });
-
-  // Start observing the page's DOM for changes
-  observer.observe(document.body, { childList: true, subtree: true });
-}
-
-// Run the replaceImages and observePage functions when the page loads
+// Run the replaceImages function when the page loads
 window.onload = function() {
   replaceImages();
-  observePage();
+
+  // Observe changes to the DOM and run replaceImages when changes occur
+  let observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.type === 'childList') {
+        replaceImages();
+      }
+    });
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
 };
